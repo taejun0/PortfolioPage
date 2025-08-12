15695ddadc7b8ef60178e31e17e9bd1d0608092f
taejun0 <ohtaetaejun@gmail.com>
2025-08-13 00:09:16 +0900

    [Refactor] #14 - AboutPage 상수화

---
+++ src/features/aboutpage/components/third/ThirdConstant.ts
@@ -0,0 +1,83 @@
+export const THIRDCONSTANT = {
+  TEXT: {
+    TITLE: "My Journey",
+    SEMITITLE: "지금까지 저를 만들어준 경험을 보실 수 있습니다",
+    CONTENT: {
+      ONE: {
+        TIME: "2020",
+        TITLE: "코로나와 함께한 대학 입학",
+        CONTENT: {
+          TITLE: "동국대학교 입학",
+          TEXT: [
+            "다빈치, 공학교육혁신센터 선발",
+            "코로나19로 인한 비대면 학기 경험",
+          ],
+        },
+      },
+      TWO: {
+        TIME: "2021",
+        TITLE: "개발의 문을 열다",
+        CONTENT1: {
+          TITLE: "동국대학교 학술 창업 동아리, CAPS 34기 기획부원",
+          TEXT: ["HTML 및 CSS 스터디"],
+        },
+        CONTENT2: {
+          TITLE: "다빈치, 공학교육 혁신 센터 주관 프로그램 수료",
+          TEXT: [
+            "2021 IoT 스마트 메이커스 프로그램 수료",
+            "2021 동국대 2학기 코테 대비 캠프 수료",
+          ],
+        },
+        CONTENT3: "코로나19로 인한 비대면 학기 경험",
+        KEYWORD: ["HTML", "CSS", "IoT"],
+      },
+      THREE: {
+        TIME: "2022 - 2023",
+        TITLE: "2년간의 군 복무",
+        CONTENT1: {
+          TEXT: ["병장 만기 전역", "서비스 정신과 책임감을 되새긴 시간"],
+        },
+      },
+      FOUR: {
+        TIME: "2024",
+        TITLE: "본격적인 도전의 해",
+        CONTENT1: {
+          TITLE: "DB 드림리더",
+          TEXT: ["ESG 중 환경 분야에 대한 문제 도출 및 솔루션 진행"],
+        },
+        CONTENT2: {
+          TITLE: "IT 창업 연합동아리, 멋쟁이사자처럼 12기",
+          TEXT: [
+            "프론트엔드 트랙원 및 홍보팀장",
+            "교내 해커톤 '심바톤' 2등상 수상",
+            "멋쟁이사자처럼 중앙해커톤 참가",
+            "대학 연합 해커톤 '4호선톤' 입선 (5위)",
+          ],
+        },
+        CONTENT3: "코로나19로 인한 비대면 학기 경험",
+        KEYWORD: ["React", "JavaScript", "Node.js"],
+      },
+      FIVE: {
+        TIME: "2025",
+        TITLE: "성장과 확장의 시기",
+        CONTENT1: {
+          TITLE: "IT 창업 연합동아리, 멋쟁이사자처럼 13기",
+          TEXT: [
+            "프론트엔드 트랙장",
+            "동국대학교 동아리 박람회 사이트 '동아리랑' 개발",
+            "축제 부스 운영 사이트 'D-order' 프론트 팀장",
+          ],
+        },
+        CONTENT2: {
+          TITLE: "(주) 서큘러스 프리랜서",
+          TEXT: ["WCRC 로봇대회 멘토링", "교육 콘텐츠 개발"],
+        },
+        CONTENT3: {
+          TITLE: "한국 통신 학회",
+          TEXT: ["2025년 하계종합학술발표회 아이디어경진대회 후원업체상"],
+        },
+        KEYWORD: ["Leadership", "TypeScript", "Next.js"],
+      },
+    },
+  },
+};
