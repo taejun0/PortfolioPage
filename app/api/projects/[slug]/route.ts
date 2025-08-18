2c83c695c9a5109a88f342954d9da129f6928958
taejun0 <ohtaetaejun@gmail.com>
2025-08-18 23:59:01 +0900

    [HotFix] 초기 상태로 돌리기

--- app/api/projects/[slug]/route.ts
+++ app/api/projects/[slug]/route.ts
@@ -4,9 +4,11 @@ import { getProjectBySlug } from "@lib/projects";
 
 export const revalidate = 60;
 
-export async function GET(_req: Request, context: any) {
-  const { slug } = context.params as { slug: string };
-  const data = await getProjectBySlug(slug);
+export async function GET(
+  _: Request,
+  { params }: { params: { slug: string } }
+) {
+  const data = await getProjectBySlug(params.slug);
   if (!data) return new NextResponse("Not Found", { status: 404 });
   return NextResponse.json(data);
 }
