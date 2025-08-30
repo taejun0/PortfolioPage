b87c396ead0bcc636a4002aa5c12a3747de4a6c3
taejun0 <ohtaetaejun@gmail.com>
2025-08-30 13:32:33 +0900

    Refactor #21 - 디자인 수정

--- src/features/mainpage/components/second/SecondPage.tsx
+++ src/features/mainpage/components/second/SecondPage.tsx
@@ -10,20 +10,40 @@ const SecondPage = () => {
         <S.Container1>
           <S.AboutTitle>About ME</S.AboutTitle>
           <S.AboutText>
-            {`소통, 경험, 협업
-
-          FrontEnd는 화면(View)을 통해 사용자와 가장 먼저 소통합니다.
-          복잡한 내용을 명확하게 전달하고,  
-          사용자 흐름에 따라 자연스럽게 이어지는 화면을 설계합니다.
-
-          단순한 UI가 아닌 머무르고 싶은 경험을 만드는 것이 목표입니다.  
-          한 줄의 문구, 한 픽셀의 마진에도 이유를 담고
-          사용자의 입장에서 반복해 고민합니다.
-
-          저는 혼자일 때보다 함께일 때
-          더 나은 결과가 만들어진다고 믿습니다.
-          팀원들과 끊임없이 대화하고, 설득하고, 조율하며  
-          모두가 납득할 수 있는 방향으로 나아가고자 합니다.`}
+            <span className="highlight">소통</span>,&nbsp;
+            <span className="highlight">경험</span>,&nbsp;
+            <span className="highlight">협업</span>
+            <hr />
+            <br />
+            FrontEnd는 <span className="highlight">화면(View)</span>을 통해
+            <br />
+            사용자와 가장 먼저 소통합니다.
+            <br />
+            <br />
+            복잡한 내용을 명확하게 전달하고,
+            <br />
+            <span className="highlight">사용자 흐름</span>에 따라 자연스럽게
+            이어지는 화면을 설계합니다.
+            <br />
+            <br />
+            단순한 UI가 아닌{" "}
+            <span className="highlight">머무르고 싶은 경험</span>을
+            <br /> 만드는 것이 목표입니다.
+            <br />
+            한 줄의 문구, 한 픽셀의 마진에도 이유를 담고
+            <br />
+            <span className="highlight">사용자의 입장</span>에서 반복해
+            고민합니다.
+            <br />
+            <br />
+            저는 <span className="highlight">혼자</span>일 때보다{" "}
+            <span className="highlight">함께</span>일 때<br />더 나은{" "}
+            <span className="highlight">결과</span>가 만들어진다고 믿습니다.
+            <br />
+            팀원들과 끊임없이 대화하고, 설득하고, 조율하며
+            <br />
+            <span className="highlight">모두가 납득할 수 있는 방향</span>으로
+            나아가고자 합니다.
           </S.AboutText>
           <Link href={ROUTE_CONSTANTS.ABOUT}>
             <S.GoAboutMe>저에 대해 자세히 알아봐요 {"->"}</S.GoAboutMe>
@@ -36,7 +56,6 @@ const SecondPage = () => {
             width={640}
             height={800}
             sizes="(max-width: 768px) 100vw, 400px"
-            style={{ paddingRight: 20 }}
             priority
           />
         </S.Container2>
