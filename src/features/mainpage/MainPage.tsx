4112841919228bb9eab649d5f76a04240a608543
taejun0 <ohtaetaejun@gmail.com>
2026-01-23 10:00:00 +0900

    refactor: Remove StickyAvatar component from MainPage

--- src/features/mainpage/MainPage.tsx
+++ src/features/mainpage/MainPage.tsx
@@ -3,8 +3,6 @@
 import * as S from "./MainPage.styled";
 import dynamic from "next/dynamic";
 import FirstPage from "./components/first/FirstPage";
-import { useRef } from "react";
-import StickyAvatar from "./components/stickyavatar.tsx/StickyAvatar";
 
 // 첫 화면은 즉시 로드, 나머지는 동적 임포트
 const SecondPage = dynamic(() => import("./components/second/SecondPage"), {
@@ -25,16 +23,13 @@ const SixPage = dynamic(() => import("./components/six/SixPage"), {
 });
 
 const MainPage = () => {
-  const secondImageRef = useRef<HTMLDivElement>(null);
-
   return (
     <S.Wrapper data-scroll-root>
-      <StickyAvatar targetRef={secondImageRef} />
       <S.Section id="main">
         <FirstPage />
       </S.Section>
       <S.Section id="about">
-        <SecondPage profileRef={secondImageRef} />
+        <SecondPage />
       </S.Section>
       <S.Section id="skills">
         <ThirdPage />
